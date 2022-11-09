6445b299295619076171343d8931a5e8538d4c47
Talal-12 <talal.alothman@gmail.com>
2022-11-09 14:38:40 +0300

    DONE

--- main.dart
+++ main.dart
@@ -1,7 +1,20 @@
-void main(){
+void main() {
   // Write your code here
-  
-  
-  
-  
+  List<int> numbers = [5, 3, 15, 4, 1];
+  int sum = 0;
+  numbers.forEach((number) => sum += number);
+  print('Sum: ${sum}');
+
+  int largestValue = numbers[0];
+  numbers.forEach((number) => {
+        if (number > largestValue) {largestValue = number}
+      });
+  print('The largest value is: ${largestValue}');
+
+  List<int> ages = [8, 15, 10, 24, 5];
+  List<int> filterVisitors = ages.where((age) => age >= 12).toList();
+  print(filterVisitors);
+
+  int findOdds = numbers.firstWhere((number) => number.isOdd, orElse: () => 0);
+  print(findOdds);
 }
